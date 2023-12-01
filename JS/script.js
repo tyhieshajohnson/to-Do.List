980f0bc8baa5f0eb71ce5cc6ccc48716ef87c4d3
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-12-01 14:51:45 +0200

    finalSubmission

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,15 @@
 // An array to store the to-do list items
-let toDoList = [];
+let myToDoList = [];
 
 // Renders the to-do list on the web page
 function renderList() {
     // Global variable for the list container
-    const listContainer = document.getElementById("toDoList");
+    const listContainer = document.getElementById("myToDoList");
     // Clears the existing content to re-render from scratch
     listContainer.innerHTML = "";
 
     // Iterates through each to-do list item
-    toDoList.forEach((item) => {
+    myToDoList.forEach((item) => {
         // Creates a new list item element using JavaScript
         const listItem = document.createElement("li");
 
@@ -19,7 +19,7 @@ function renderList() {
         checkbox.checked = item.completed;
         checkbox.style.textDecoration = "line-through";
         checkbox.className = "checkbox";
-        checkbox.addEventListener("change", () => toggleComplete(item.id));
+        checkbox.addEventListener("change", () => myToggleComplete(item.id));
 
         // Creates a span element for the task name
         const itemName = document.createElement("span");
@@ -34,7 +34,7 @@ function renderList() {
         const deleteIcon = document.createElement("span");
         deleteIcon.innerHTML = "&times";
         deleteIcon.className = "delete";
-        deleteIcon.addEventListener("click", () => deleteItem(item.id));
+        deleteIcon.addEventListener("click", () => myDeleteItem(item.id));
 
         // Appends elements to the list item
         listItem.appendChild(checkbox);
@@ -48,77 +48,84 @@ function renderList() {
 }
 
 // Adds a new item to the to-do list
-function addItem() {
+function myAddItem() {
     // Retrieves the task name input element
-    const taskNameInput = document.getElementById("taskName");
+    const taskNameInput = document.getElementById("myTaskName");
     const taskName = taskNameInput.value.trim();
 
     // Validates the task name
-    if (isValidTask(taskName)) {
+    if (myIsValidTask(taskName)) {
         // Creates a new to-do list item
         const newItem = {
             id: Date.now(),
+            // This line adds a property to the newItem object with the key name. 
+            // charAt targets the character of the index.
+            // slice = targets the index extracts a portion from a list
             name: taskName.charAt(0).toUpperCase() + taskName.slice(1),
             createdDate: new Date().toLocaleDateString(),
             completed: false,
         };
 
         // Adds a click event listener to the sort button
-        let sortBtn = document.getElementById('sortButton');
-        sortBtn.addEventListener('click', sortTasks);
-        let isSorted = true;
+        let sortBtn = document.getElementById('mySortButton');
+        sortBtn.addEventListener('click', mySortTasks);
+        let isSorted = false;
 
-        // Sorts the to-do list based on the task name
-        function sortTasks() {
+        function mySortTasks() {
             isSorted = !isSorted;
             if (isSorted) {
-                toDoList.sort((a, b) => (a.name > b.name ? 1 : -1));
+                myToDoList.sort((a, b) => (a.name > b.name ? 1 : -1)); // Sort alphabetically
             } else {
-                toDoList.sort((a, b) => (a.name < b.name ? 1 : -1));
+                myToDoList.sort((a, b) => (a.name < b.name ? 1 : -1)); //  reverse alphabetical order
             }
-            renderList();
+            // NOT FUNCTIONING :(
         }
 
         // Adds the new item to the to-do list, saves to local storage, and re-renders the list
-        toDoList.push(newItem);
-        saveTasksToLocalStorage();
+        myToDoList.push(newItem);
+        mySaveTasksToLocalStorage();
         renderList();
         taskNameInput.value = "";
     } else {
-        alert("Please enter a valid task name (more than 3 characters, starting with an uppercase letter).");
+        alert("Start Task with Uppercase!");
     }
 }
 
+const addItemButton = document.getElementById("myAddItemButton");
+
+// Add an event listener for the "click" event
+addItemButton.addEventListener("click", myAddItem);
+
 // Checks if a task name is valid
-const isValidTask = (name) => {
+const myIsValidTask = (name) => {
     return name !== "" && name.length > 3 && /^[A-Z]/.test(name);
 };
 
 // Toggles the completion status of a to-do list item
-function toggleComplete(itemId) {
-    const item = toDoList.find((item) => item.id === itemId);
+function myToggleComplete(itemId) {
+    const item = myToDoList.find((item) => item.id === itemId);
     if (item) {
         item.completed = !item.completed;
-        saveTasksToLocalStorage();
+        mySaveTasksToLocalStorage();
         renderList();
     }
 }
 
 // Deletes a to-do list item
-function deleteItem(itemId) {
-    toDoList = toDoList.filter((item) => item.id !== itemId);
-    saveTasksToLocalStorage();
+function myDeleteItem(itemId) {
+    myToDoList = myToDoList.filter((item) => item.id !== itemId);
+    mySaveTasksToLocalStorage();
     renderList();
 }
 
 // Saves the to-do list to local storage
-function saveTasksToLocalStorage() {
-    localStorage.setItem("tasks", JSON.stringify(toDoList));
+function mySaveTasksToLocalStorage() {
+    localStorage.setItem("myTasks", JSON.stringify(myToDoList));
 }
 
 // Loads stored tasks from local storage on page load
 window.addEventListener("load", () => {
-    const storedTasks = localStorage.getItem("tasks");
-    toDoList = storedTasks ? JSON.parse(storedTasks) : [];
+    const storedTasks = localStorage.getItem("myTasks");
+    myToDoList = storedTasks ? JSON.parse(storedTasks) : [];
     renderList();
 });
