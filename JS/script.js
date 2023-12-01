c35f77a6e113f7c435590c02480b669bc4de95e4
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-12-01 15:12:13 +0200

    triedFixingSortByHoistingAndRemovingItFromAddItem

--- JS/script.js
+++ JS/script.js
@@ -66,24 +66,6 @@ function myAddItem() {
             completed: false,
         };
 
-        // Adds a click event listener to the sort button
-        let sortBtn = document.getElementById('mySortButton');
-        sortBtn.addEventListener('click', mySortTasks);
-        let isSorted = false;
-
-        function mySortTasks() {
-            isSorted = !isSorted;
-            myToDoList.sort((a, b) => {
-                if (isSorted) {
-                    return a.name.localeCompare(b.name); // Sort alphabetically
-                } else {
-                    return b.name.localeCompare(a.name); // Sort reverse alphabetically
-                }
-            });
-            renderList(); // Re-render the sorted list after sorting
-            // NOT FUNCTIONING :(
-        }
-
         // Adds the new item to the to-do list, saves to local storage, and re-renders the list
         myToDoList.push(newItem);
         mySaveTasksToLocalStorage();
@@ -95,6 +77,24 @@ function myAddItem() {
 }
 
 const addItemButton = document.getElementById("myAddItemButton");
+
+// Adds a click event listener to the sort button
+function mySortTasks() {
+    isSorted = !isSorted;
+    myToDoList.sort((a, b) => {
+        if (isSorted) {
+            return a.name.localeCompare(b.name); // Sort alphabetically
+        } else {
+            return b.name.localeCompare(a.name); // Sort reverse alphabetically
+        }
+    });
+    renderList(); // Re-render the sorted list after sorting
+    // NOT FUNCTIONING :(
+}
+
+let sortBtn = document.getElementById('mySortButton');
+sortBtn.addEventListener('click', mySortTasks);
+let isSorted = false;
 
 // Add an event listener for the "click" event
 addItemButton.addEventListener("click", myAddItem);
